70878b6fccc3d51e7dab1eef3545edca51c078a5
Tom von Schwerdtner <tomvons@gmail.com>
2013-07-17 20:46:31 -0400

    vanilla rails app

---
+++ widgets/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Widgets::Application.config.secret_key_base = 'f5e0c881c348bfe2d1ed276f39fc64d25c61a10a6f4ab5bfd8ecf9cb9caeb0cd7449006952098332a65a8ab603acbcbfed2c6e310d095c36d968f837140ebc01'
