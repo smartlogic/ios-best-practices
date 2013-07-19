a17e2cbf49ae7a184315670d5ab54c39eac0ec65
Tom von Schwerdtner <tomvons@gmail.com>
2013-07-19 06:33:06 -0400

    Remove stale comments!

--- BohConfDemo/BohConfDemo/AppDelegate.m
+++ BohConfDemo/BohConfDemo/AppDelegate.m
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.m
-//  BohConfDemo
-//
-//  Created by Tom von Schwerdtner on 7/17/13.
-//  Copyright (c) 2013 Tom von Schwerdtner. All rights reserved.
-//
 
 #import "AppDelegate.h"
 
